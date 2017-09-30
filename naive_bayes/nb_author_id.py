9a65003c59adc0170cc073aa4c3321be5e7de1f7
NSGoat <ed@rutterstudios.co.uk>
2017-09-30 18:33:55 +0100

    Naive Bayes email classifier solution (lesson 2)

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -22,12 +22,18 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-
-
 #########################################################
-### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
 
+train_time = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-train_time, 3), "s"
 
+pred_time = time()
+accuracy = clf.score(features_test, labels_test)
+print "prediction time:", round(time()-pred_time, 3), "s"
+print "accuracy", accuracy
 #########################################################
 
 
