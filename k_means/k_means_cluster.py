652efca3f7d908f069f25875f7b69ed0c7cf179f
NSGoat <ed@rutterstudios.co.uk>
2017-10-05 19:57:00 +0100

    Cluster data using K Means clustering (lesson 9)

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -65,7 +65,11 @@ plt.show()
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+from sklearn.cluster import KMeans
 
+kmeans = KMeans(n_clusters=2, n_init=10, max_iter=300)
+kmeans.fit(data)
+pred = kmeans.labels_
 
 
 ### rename the "name" parameter when you change the number of features
