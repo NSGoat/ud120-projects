b5015f18aea70549c6862a209c94a870eeb4b9ff
NSGoat <ed@rutterstudios.co.uk>
2017-10-05 15:09:59 +0100

    Remove outliers and refit LinearRegression (lesson 8)

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,20 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
+    # Create list of tuples from NumPy arrays
+    # predictions = [[123], [456], [789], ...]
+    # ages = [[1.23], [4.56], [7.89], ...]
+    # net_worths = [[1], [4], [7], ...]
+    # ~= [(123, 1.23, 1), (456, 4.56, 4), (789, 7.89, 7)...]
+    zipped_data = zip(predictions.tolist(), ages.tolist(), net_worths.tolist())
+    sanitised_data = map(lambda x: (x[0][0], x[1][0], x[2][0]), zipped_data)
 
-    ### your code goes here
+    # Reform data into expect return shape
+    output_tuples = map(lambda x: (x[1], x[2], abs(x[0] - x[2])), sanitised_data)
 
-    
-    return cleaned_data
+    # Sort data by difference between prediction and net_worth stored in tuple
+    output_tuples = sorted(output_tuples, key=lambda x: x[2])
 
+    # Return 10 percent of points with greatest error
+    elements_to_return = int(len(output_tuples) * 0.9)
+    return output_tuples[:elements_to_return]
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,14 +26,12 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
 
-
-
-
-
-
-
-
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print "Slope:", reg.coef_
+print "Score:", reg.score(ages_test, net_worths_test)
 
 
 try:
@@ -42,6 +40,7 @@ except NameError:
     pass
 plt.scatter(ages, net_worths)
 plt.show()
+# plt.savefig("Regression.png")
 
 
 ### identify and remove the most outlier-y points
@@ -58,7 +57,6 @@ except NameError:
 
 
 
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -68,7 +66,7 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
-        plt.plot(ages, reg.predict(ages), color="blue")
+        plt.plot(ages, reg.predict(ages), color="green")
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
@@ -77,8 +75,11 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
+    # plt.savefig("RegressionRemovingOutliers.png")
 
+    print "Items in cleaned data:", len(cleaned_data)
+    print "Slope:", reg.coef_
+    print "Score:", reg.score(ages_test, net_worths_test)
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
