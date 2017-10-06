f45d7eb0db1e4fe21c9ec6b35e0ea66dd8acd927
NSGoat <ed@rutterstudios.co.uk>
2017-10-06 14:44:33 +0100

    Stem words in email parser (lesson 11)

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,15 +28,21 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
 
 
+        stemmer = SnowballStemmer("english")
 
+        words_to_stem = text_string.split()
+        stemmed_words = []
+        # words = ""
+        for word in words_to_stem:
+            words += stemmer.stem(word)
+            words += " "
 
     return words
 
