bc979a1c052d16089f2c358bfc2c9f5ea42c66fa
NSGoat <ed@rutterstudios.co.uk>
2017-10-06 13:51:13 +0100

    Remove total payments feature and perform feature scaling on salary and exercised_stock_options (lesson 10)

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -36,7 +36,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.savefig(name)
     plt.show()
 
-
+print "\nBeginning"
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
@@ -53,27 +53,36 @@ print key_min_stock, data_dict[key_min_stock]["exercised_stock_options"], "exerc
 print key_max_stock, data_dict[key_max_stock]["exercised_stock_options"], "exercised_stock_options max"
 
 # Filter out items that are missing values for salary
-data_dict = {k: v for k, v in data_dict.items() if v["salary"] != "NaN" }
+# data_dict = {k: v for k, v in data_dict.items() if v["salary"] != "NaN" }
 
 # Find min and max saleries
 key_min_salary = min(data_dict, key=lambda k: data_dict[k]["salary"])
 key_max_salary = max(data_dict, key=lambda k: data_dict[k]["salary"])
 print key_min_salary, data_dict[key_min_salary]["salary"], "salary min"
-print key_max_salary, data_dict[key_max_salary]["salary"], "salary max"
+print key_max_salary, data_dict[key_max_salary]["salary"], "salary max\n"
+
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
-feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2, feature_3]
+features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+# Perform feature scaling on salary and exercised_stock_options
+finance_features.append(numpy.array([200000., 1000000]))
+print "Quiz",  finance_features[-1]
+print finance_features
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+finance_features = scaler.fit_transform(finance_features)
+print "Min:", scaler.data_min_, "Max:", scaler.data_max_
+print "Quiz", finance_features[-1]
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2, _ in finance_features:
+for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
